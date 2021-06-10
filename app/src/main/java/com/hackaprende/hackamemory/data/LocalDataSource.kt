ebb0756d4f3a814a0b04b2ce7c863fc526e4d078
Jesus Almaral <almaral17@gmail.com>
2021-06-10 16:22:28 -0500

    Use LocalDataSource to generate cards

---
+++ app/src/main/java/com/hackaprende/hackamemory/data/LocalDataSource.kt
@@ -0,0 +1,40 @@
+package com.hackaprende.hackamemory.data
+
+import com.hackaprende.hackamemory.R
+import com.hackaprende.hackamemory.game.MemoryCard
+
+class LocalDataSource {
+
+    fun getCardsForGame(numberOfRows: Int, numberOfColumns: Int): MutableList<MemoryCard> {
+        val allCards = generateAllCards()
+
+        val cardList = allCards.subList(0, numberOfRows * numberOfColumns)
+        cardList.shuffle()
+        return cardList
+    }
+
+    private fun generateAllCards(): MutableList<MemoryCard> {
+        val cardList = mutableListOf<MemoryCard>()
+        cardList.add(MemoryCard(R.drawable.memory_bat_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_bat_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_cat_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_cat_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_cow_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_cow_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_dragon_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_dragon_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_garbage_man_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_garbage_man_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_ghost_dog_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_ghost_dog_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_hen_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_hen_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_horse_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_horse_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_pig_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_pig_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_spider_card_front))
+        cardList.add(MemoryCard(R.drawable.memory_spider_card_front))
+        return cardList
+    }
+}
--- app/src/main/java/com/hackaprende/hackamemory/game/GameRepository.kt
+++ app/src/main/java/com/hackaprende/hackamemory/game/GameRepository.kt
@@ -1,39 +1,10 @@
 package com.hackaprende.hackamemory.game
 
-import com.hackaprende.hackamemory.R
+import com.hackaprende.hackamemory.data.LocalDataSource
 
 class GameRepository {
 
     fun getCardsForGame(numberOfRows: Int, numberOfColumns: Int): MutableList<MemoryCard> {
-        val allCards = getAllCards()
-
-        val cardList = allCards.subList(0, numberOfRows * numberOfColumns)
-        cardList.shuffle()
-        return cardList
-    }
-
-    private fun getAllCards(): MutableList<MemoryCard> {
-        val cardList = mutableListOf<MemoryCard>()
-        cardList.add(MemoryCard(R.drawable.memory_bat_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_bat_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_cat_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_cat_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_cow_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_cow_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_dragon_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_dragon_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_garbage_man_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_garbage_man_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_ghost_dog_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_ghost_dog_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_hen_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_hen_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_horse_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_horse_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_pig_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_pig_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_spider_card_front))
-        cardList.add(MemoryCard(R.drawable.memory_spider_card_front))
-        return cardList
+        return LocalDataSource().getCardsForGame(numberOfRows, numberOfColumns)
     }
 }
